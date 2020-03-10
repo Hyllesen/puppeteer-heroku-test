a5140a684f0c14ba06c66e25686bf73721421864
Hyllesen <stefanhyltoft@gmail.com>
2020-03-10 22:18:27 +0100

    Headless true now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 async function main() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const tab = await browser.newPage();
   const text = await (await tab.goto("https://myip.dk")).text();
   console.log(text);
